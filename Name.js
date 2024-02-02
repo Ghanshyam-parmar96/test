1f8bd1caa1817bc9b9c9682c8c6f2a10ae051100
Ghanshyam parmar <pghanshyam450@gmail.com>
2024-02-02 21:05:26 +0530

    change variabal name

--- Name.js
+++ Name.js
@@ -1,5 +1,5 @@
-let  name = "Ghanshyam parmar"
-console.log(name)
+let  myName = "Ghanshyam parmar"
+console.log(myName )
 let father = "Bhagwat singh parmar"
 console.log(father)
 let mother = "Chinta mani parmar"
